a71678ddf493ea4602e2f27e9e332cef064e7fb0
Aanshi R kumar <96661837+annz-gif@users.noreply.github.com>
2024-03-31 01:35:31 +0530

    Add files via upload

---
+++ house price prediction.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+data=pd.read_csv('C:/Users/aansh/downloads/Bengaluru_House_Data.csv')
+
+
+# In[3]:
+
+
+data.head()
+
+
+# In[5]:
+
+
+data.shape
+
+
+# In[ ]:
+
+
+
+
